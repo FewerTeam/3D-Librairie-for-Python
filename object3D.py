25aaafbcdc33b92bae9071ce9dd69722b2b71ff7
FewerElk <gabriel@trouilhet.fr>
2023-02-18 11:58:08 +0100

    modification

--- object3D.py
+++ object3D.py
@@ -12,6 +12,7 @@ class Object3D(object):
         - points : list of tuples with 3 coord.
         - id : the id is attributed by the Screen class. DO NOT TOUCH, exceept you know what you are doing !"""
         self.list_points = points
+        self.list_edge = []
         self.color = color
         if id_ == "DO NOT TOUCH IT !":
             raise AttributeError("Sorry, but you must give this argument (id_). For it, you have to use Screen.get_id(), it gener id.")
@@ -31,3 +32,19 @@ class Object3D(object):
     def give_top(self, oy, oz):
         """Give the order of priority of the points."""
         pass
+
+    def create_edge(self, points):
+        """Create a new edge.
+        Arguments :
+        - points : a list with 2 points."""
+
+    def create_face(self, points, skin=("color", "blue")):
+        """Create a new face.
+        Arguments : 
+        - points : a list of points
+        - a tuple with the type of the skin of the face, and the skin. Default : type:color and skin:blue."""
+
+class Cube(Object3D):
+    def __init__(self, points, color, id_='DO NOT TOUCH IT !'):
+        super(Object3D, self).__init__(points, color, id_)
+        

ed4f78dadb95bc1af619aa0810125627a733442b
FewerElk <gabriel@trouilhet.fr>
2023-02-20 12:13:17 +0100

    Move ?

--- screen.py
+++ screen.py
@@ -117,18 +117,25 @@ class Screen(object):
         self.last_x = x
         self.last_y = y
 
+        self.reload()
+        self.allow_move()
+
     
     def move_l(self, value):
         print("I go to the left with value {0}".format(value))
+        self.orient_y += value
 
     def move_r(self, value):
         print("I go to the right with value {0}".format(value))
+        self.orient_y -= value
 
     def move_u(self, value):
         print("I go to the up with value {0}".format(value))
+        self.orient_z += value
 
     def move_d(self, value):
         print("I go to the down with value {0}".format(value))
+        self.orient_z -= value
 
     def mainloop(self):
         mainloop()
@@ -188,7 +195,7 @@ class Screen(object):
         """Convertise a 3d point to a 2d point with the rules of the isometric perspective.
         Arguments : 
         - point3d : the point3d who will be convertised."""
-        return (point3d[0]**(point3d[2]/10), point3d[1]**(point3d[2]/10))         #will be changed, it is for a test.
+        return (point3d[0]*(point3d[2]/(self.orient_z+5.5)), point3d[1]*(point3d[2]/(self.orient_y+5.5)))         #will be changed, it is for a test.
 
     def _convertise_humain(self, point3d):
         """Convertise a 3d point to a 2d point with the rules of the "humain" perspective.
@@ -231,5 +238,6 @@ if __name__ == "__main__":
     module = Screen(410, 410, "yo le test")
     a = object3D.Cube([(10, 10, 10), (20, 10, 10), (20, 10, 20), (10, 10, 20), (10, 20, 20), (10, 20, 10), (20, 20, 10), (20, 20, 20)], color="blue", id_=module.get_id())
     module.add_object(a)
+    module.allow_move()
     module.addquitbutton("EXIT")
     module.mainloop()
